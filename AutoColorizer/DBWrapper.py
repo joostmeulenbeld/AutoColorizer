97f3a1a46f19fd17b15041f4369acdb04013cd65
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2016-05-01 21:02:36 +0200

    Added fruitfetcher and image databse wrapper

---
+++ AutoColorizer/DBWrapper.py
@@ -0,0 +1,195 @@
+﻿import urllib
+import sqlite3
+import os
+
+class DBWrapper:
+    """Wrapper for the sqlite database to add new images or get image file names"""
+
+    def __init__(self, images_folder='images', dbname='imagedb.db'):
+        """Open connection to the database and create a cursor. Standard image folder: 'images'
+        Don't forget to dbwrapper.close_connection() when done using the database
+        """
+
+        self.images_folder = images_folder
+        self.dbname = dbname
+        self.__open_connection()
+
+    def __open_connection(self):
+        """Open connection to database if not already opened. This function is already
+        called by the constructor of this class.
+        """
+        # check if the database exists, otherwise create the table
+        create_table = not os.path.isfile(self.dbname)
+
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+        if create_table:
+            self.setup_database()
+
+    def clear_database(self, reallydoit=False):
+        """Delete all contents from the database, procede with caution!
+        This will also setup a clean new database
+        """
+
+        if reallydoit:
+            print("Now deleting contents of database and re-configuring...")
+            self.cursor.execute('''DROP TABLE images''')
+            self.conn.commit()
+            self.setup_database()
+            print("Done!")
+
+        
+    def close_connection(self):
+        """Close the database connection"""
+
+        self.conn.close()
+
+    def setup_database(self):
+        """Call this function if no database has been setup yet"""
+
+        self.cursor.execute('''CREATE TABLE images
+            (id text, title text, flickr_url text, checked integer, good integer);''')
+        self.conn.commit()
+        return self.cursor.fetchall()
+
+    def count_images(self, bool_checked=2, bool_good=2):
+        """Returns the number of images in the database
+        The two bools: 
+            0: no, 
+            1: yes, 
+            2: doesn't matter
+        """
+        if bool_checked not in [0,1,2] or bool_good not in [0,1,2]:
+            print("Boolean should be in range [0,1,2]")
+            return -1
+
+        query = "SELECT Count(*) FROM images"
+        if bool_checked in [0,1] or bool_good in [0,1]:
+            query += " WHERE "
+            if bool_checked in [0,1]:
+                query += "checked=" + str(bool_checked)
+            if bool_checked in [0,1] and bool_good in [0,1]:
+                query += " AND "
+            if bool_good in [0,1]:
+                query += "good=" + str(bool_good)
+        query += ";"
+
+        self.cursor.execute(query)
+        return self.cursor.fetchone()[0]
+
+    def get_jpg_filenames(self, bool_checked=2, bool_good=2, limit=-1):
+        """Returns a list of all relative paths to jpg files
+        INPUT:
+            The two bools specify whether to get only checked or good files: 
+                0: no, 
+                1: yes, 
+                2: doesn't matter
+            limit=-1: set the amount of file names to retrieve, limit=-1 doesn't limit amount
+
+        OUTPUT:
+        List of tuples: [(id, path/to.jpg, checked, good), ...]
+        """
+
+        query = '''SELECT id, checked, good FROM images'''
+
+        if bool_checked in [0,1] or bool_good in [0,1]:
+            query += " WHERE "
+            if bool_checked in [0,1]:
+                query += "checked=" + str(bool_checked)
+            if bool_checked in [0,1] and bool_good in [0,1]:
+                query += " AND "
+            if bool_good in [0,1]:
+                query += "good=" + str(bool_good)
+        if limit > -1:
+            query += " LIMIT " + str(limit)
+        query += ";"
+
+        self.cursor.execute(query)
+        results = [(id, self.path_to_image(id), checked, good) for (id, checked, good) in self.cursor]
+        return results
+
+    def get_flickr_url(self, id):
+        """Return the flickr URL of a given image, identified by its id"""
+
+        self.cursor.execute('''SELECT flickr_url FROM images WHERE id=?;''', (id,))
+        return self.cursor.fetchone()[0]
+
+
+    def add_image(self, id, title, flickr_url, checked=0, good=0):
+        """Add an image to the database. Defaults to the unchecked state
+        IMPORTANT: returns the path under which to save the image!
+        """
+
+        checked = 0
+        good = 0
+        query = '''INSERT INTO images (id, title, flickr_url, checked, good) VALUES (?,?,?,?,?)'''
+        data = (id, title, flickr_url, checked, good)
+        self.cursor.execute(query, data)
+        self.conn.commit()
+        return self.path_to_image(id)
+
+    def image_in_db(self, id):
+        """Return 1 if an image ID is already in the database, 0 if it is not"""
+        self.cursor.execute('''SELECT Count(*) FROM images WHERE id=?''', (id,))
+        return self.cursor.fetchone()[0] != 0
+
+    def set_checked(self, idlist, checked=1):
+        """Set all images by ID checked, 1=checked, 0=not checked"""
+        if checked not in [0,1]:
+            print("checked value should be in [0,1]")
+            return
+
+        query = "UPDATE images SET checked={} WHERE id=?".format(str(checked))
+        idlist = [(id,) for id in idlist]
+        self.cursor.executemany(query, idlist)
+
+    def path_to_image(self, id, extension='jpg'):
+        """Generate the path to an image based on its id, defaulting to .jpg"""
+        return os.path.join(self.images_folder, id + '.' + extension)
+
+    def set_good(self, idlist, good=0):
+        """Set all images by ID good, 1=good, 0=not good"""
+        if good not in [0,1]:
+            print("checked value should be in [0,1]")
+            return
+
+        query = "UPDATE images SET good={} WHERE id=?".format(str(good))
+        idlist = [(id,) for id in idlist]
+        self.cursor.executemany(query, idlist)
+
+    def check_integrity(self, clean=False):
+        """Check if all image files are in the database and all database entries have a file
+        Set clean=True if you want to remove entries from database and files from directory if they don't match
+        """
+        #prepare the lists of files and database entries
+        idlist_db = [id for (id,) in self.cursor.execute("SELECT id FROM images")]
+        idlist_files = [os.path.splitext(filename)[0] for filename in os.listdir(self.images_folder)]
+        #compare them and get exclusive ones
+        db_not_in_files = set(idlist_db)-set(idlist_files)
+        files_not_in_db = set(idlist_files)-set(idlist_db)
+        #report
+        print("Number of files not in databse: " + str(len(files_not_in_db)))
+        print("Number of entries without file: " + str(len(db_not_in_files)))
+        #clean entries from database and files
+        if clean:
+            for id in files_not_in_db:
+                path = self.path_to_image(id)
+                print("Remove file: " + path)
+                os.remove(path)
+            for id in db_not_in_files:
+                print("Remove from database: " + id)
+            self.cursor.executemany("DELETE FROM images WHERE id=?", [(i,) for i in db_not_in_files])
+
+
+
+if __name__ == "__main__":
+    wrapper = DBWrapper(dbname='imagedb.db')
+    
+    # Check integrity of the database:
+    wrapper.check_integrity(clean=True)
+
+    #Get paths to files:
+    #print(wrapper.get_jpg_filenames(bool_checked=0, limit=4))
+
+    #Update certain parts, idlist contains a list of ID's [ID1, ID2 etc]
+    #wrapper.set_checked(idlist, checked=1)
---
+++ AutoColorizer/fruitfetcher.py
@@ -0,0 +1,111 @@
+﻿import flickrapi
+from pprint import pprint
+import string
+import urllib.request
+import math
+from datetime import date, timedelta
+from DBWrapper import DBWrapper
+import getapikey
+
+class FruitFetcher:
+    """Download images from flickr, save them to jpg files and save them to the databse"""
+
+    def __init__(self):
+        """Initialise fruitfetcher with the given api key and api secret"""
+        self.wrapper = DBWrapper(dbname='imagedb.db')
+
+        self.api_key, self.api_secret = getapikey.getkey()
+        self.flickr = flickrapi.FlickrAPI(self.api_key, self.api_secret, format='etree')
+
+
+    def __printable(self, s):
+        """Remove all non-printable characters from the array (some photo titles have
+        strange characters)
+        """
+
+        return ''.join(filter(lambda x: x in string.printable, s))
+
+    def flickr_url(self, photo, image_size='m'):
+        """Construct the static flickr url with which you can download the photo
+        Input:
+            photo object
+            image_size: see https://www.flickr.com/services/api/misc.urls.html
+        Output:
+            flickr static url
+        """
+        # return the url and the id of the photo
+        return 'https://farm{farm_id}.staticflickr.com/{server_id}/{id}_{secret}_{imsize}.jpg'.format(farm_id=1, # Default to farm 1
+            server_id=photo.get('server'), 
+            id=photo.get('id'), 
+            secret=photo.get('secret'), 
+            imsize=image_size)
+
+
+    def download_images(self, text='fruit', num_images=100000):
+        """Download images with the given text, sorted on relevance
+        to make sure best images are downloaded first
+        for every image, first the database is checked if the image is not already there.
+        Then it is added to the database, then it is saved to storage.
+        INPUT:
+            text='fruit': text to search for
+            num_images=100000: amoung of images to download
+        OUTPUT:
+            number of photos added
+        """
+
+        #Get the current amount of photos in the database to calculate the
+        #amount of downloaded images afterwards
+        counter = 0
+        number_of_photos_old = self.wrapper.count_images()
+
+        final_week_day = date.today()
+        #loop over amount of weeks to go back in time (10 years)
+        for weeksback in range(0,520,1):
+
+            if counter > num_images:
+                break
+
+            final_week_day -= timedelta(days=7)
+            begin_week_day = final_week_day - timedelta(days=6)
+
+
+            for photo in self.flickr.walk(tag_mode='all',
+                    text=text,
+                    sort='relevance', #relevant photos first, otherwise the result is crap
+                    min_taken_date=begin_week_day.isoformat(),
+                    max_taken_date=final_week_day.isoformat(),
+                    per_page=100):
+
+                if counter > num_images:
+                    break
+
+                # Check if the photo ID is already in the database.  If so, go
+                # to the previous week
+                if self.wrapper.image_in_db(photo.get('id')):
+                    print("\tImage already exists: " + photo.get('id') + ", going to previous week")
+                    break
+                else:
+                    counter += 1
+                    progress = ("({:" + str(2 + math.ceil(math.log10(num_images))) + 'd}/' + str(num_images) + ')').format(counter)
+
+                    url = self.flickr_url(photo)
+                    # Add the image to the database (function returns the disk
+                    # location where to store the image)
+                    filename = self.wrapper.add_image(photo.get('id'), photo.get('title'), url)
+                    urllib.request.urlretrieve(url, filename)  #Download the file to the local folder
+                
+                    print(progress + " Image downloaded: " + photo.get('id'))
+
+        #Get the new amount of photos in database and report amount of
+        #downloaded images
+        number_of_photos_new = self.wrapper.count_images()
+        return (number_of_photos_new - number_of_photos_old)
+
+
+
+if __name__ == "__main__":
+    ff = FruitFetcher()
+    #ff.wrapper.clear_database(reallydoit=True)
+    print("Number of downloaded images: " + str(ff.download_images()))
+    #ff.wrapper.check_integrity(clean=False)
+    print(ff.wrapper.count_images(bool_checked=1))
