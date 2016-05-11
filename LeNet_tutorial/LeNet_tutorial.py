3e66c446c5581ee84e63b271ad28442fd9fde80f
joost dorscheidt <j.l.dorscheidt@student.tudelft.nl>
2016-05-11 21:43:41 +0200

    added some files

---
+++ LeNet_tutorial/LeNet_tutorial.py
@@ -0,0 +1,55 @@
+import theano
+from theano import tensor as T
+from theano.tensor.nnet import conv2d
+import matplotlib.pylab as pylab
+import os.path
+from PIL import Image
+
+import numpy
+
+rng = numpy.random.RandomState(2355)
+
+
+# instantiate 4D tensor for input
+input = T.tensor4(name='input')
+
+# initialize shared variable for weights.
+w_shp = (2, 3, 9, 9)
+w_bound = numpy.sqrt(3 * 9 * 9)
+W = theano.shared( numpy.asarray(
+            rng.uniform(
+                low=-1.0 / w_bound,
+                high=1.0 / w_bound,
+                size=w_shp),
+            dtype=input.dtype), name ='W')
+
+b_shp = (2,)
+b = theano.shared(numpy.asarray(
+            rng.uniform(low=-.5, high=.5, size=b_shp),
+            dtype=input.dtype), name ='b')
+
+conv_out = conv2d(input, W)
+
+output = T.nnet.sigmoid(conv_out + b.dimshuffle('x', 0, 'x', 'x'))
+
+# create theano function to compute filtered images
+f = theano.function([input], output)
+
+# open random image of dimensions 639x516
+imopen = open('/Users/joostdorscheidt/Desktop/3wolfmoon.jpg','rb')
+img = Image.open(imopen)
+# dimensions are (height, width, channel)
+img = numpy.asarray(img, dtype='float64') / 256.
+
+# put image in 4D tensor of shape (1, 3, height, width)
+img_ = img.transpose(2, 0, 1).reshape(1, 3, 639, 516)
+filtered_img = f(img_)
+
+# plot original image and first and second components of output
+pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
+pylab.gray()
+# recall that the convOp output (filtered image) is actually a "minibatch",
+# of size 1 here, so we take index 0 in the first dimension:
+pylab.subplot(1, 3, 2); pylab.axis('off'); pylab.imshow(filtered_img[0, 0, :, :])
+pylab.subplot(1, 3, 3); pylab.axis('off'); pylab.imshow(filtered_img[0, 1, :, :])
+pylab.show()
