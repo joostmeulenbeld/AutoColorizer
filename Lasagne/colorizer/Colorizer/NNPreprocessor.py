c7f81d930364b5a0d85d1da24b5c14ca302c714c
Joost Meulenbeld <joost.meulenbeld@gmail.com>
2016-06-01 15:06:06 +0200

    implemented _process_batch

--- Lasagne/colorizer/Colorizer/NNPreprocessor.py
+++ Lasagne/colorizer/Colorizer/NNPreprocessor.py
@@ -8,12 +8,12 @@ from queue import Queue
 
 class NNPreprocessor(object):
     """This class preprocesses the batches previously generated"""
-    
+
     def __init__(self, batch_size, folder, random_superbatches=True, blur=False, randomize=True):
         """ INPUT:
                     batch_size: amount of images returned by get batch, (needed for preloading)
-                    folder: the folder where the batches are stored (as .npy files)   
-                    random_superbatches: Open the superbatch files in a random order if True 
+                    folder: the folder where the batches are stored (as .npy files)
+                    random_superbatches: Open the superbatch files in a random order if True
                     blur: Blur the target output of every batch (obtained by get_batch) if True, (needed for preloading)
                     randomize: randomize the image order inside the superbatch if True, (needed for preloading)
         """
@@ -30,17 +30,17 @@ class NNPreprocessor(object):
         # Create queue for the superbatches
         self.superbatch_queue = Queue()
 
-        # self.filenames 
-        try: 
+        # self.filenames
+        try:
             self.filenames = os.listdir(folder)
         except:
             print("Folder does not exist")
-          
-                 
+
+
 
 
     def  get_batch(self):
-        """ OUTPUT: 
+        """ OUTPUT:
                     a batch of shape=(batch_size, 3, image_x, image_y)
         """
         # if batch array is empty, call _proces_next_superbatch, save result in self.batches
@@ -49,8 +49,8 @@ class NNPreprocessor(object):
 
         # return next item in array (pop)
         return batch_queue.get()
-        
-        
+
+
         pass
 
     def get_image(self, superbatch_id, image_id, blur=False):
@@ -58,7 +58,7 @@ class NNPreprocessor(object):
                     superbatch_id: the filenumber of the superbatch in the folder
                     image_id: The image number in the superbatch
                     blur: Blur the target output of the image if True (only performed on the CIELab colorspace output)
-            OUTPUT: 
+            OUTPUT:
                     a image in two colorspaces (CIELab and RGB) of shape=(2, 3, image_x, image_y)
                     where the first axis is: CIELab for 0 and RGB for 1
         """
@@ -73,30 +73,30 @@ class NNPreprocessor(object):
     def get_random_image(self, blur=False):
         """ INPUT:
                     blur: Blur the target output of the image if True (only performed on the CIELab colorspace output)
-            OUTPUT: 
+            OUTPUT:
                     a randomly selected image in two colorspaces (CIELab and RGB) of shape=(2, 3, image_x, image_y)
                     where the first axis is: CIELab for 0 and RGB for 1
         """
 
         # Get list of superbatch_filenames
         # Get superbatch size
-        
+
         # Generate random combination
-        
+
         # get image
-         
+
         pass
 
 
     ########## Private functions ##########
     def _process_next_superbatch(self):
-        """ 
-            Pupulate the self.batch_queue    
+        """
+            Pupulate the self.batch_queue
         """
         # if the superbatch queue has no items, repopulate superbatch queue (in random order if needed)
 
         # load superbatch numpy array by taking superbatch_queue.get()
-        
+
         # process next superbatch (get)
 
         pass
@@ -124,8 +124,32 @@ class NNPreprocessor(object):
                     processed batch (gets settings as provided by the constructor)
         """
 
-        # convert from RGB to CIELab
+        # Get the shape of the batch (batch_size, 2, image_x, image_y)
+        batch_shape = batch.shape
+        print(batch_shape)
+        # Define gaussian blur standard deviation
+        sigma = 3
 
-        # Blur if needed
+        # Loop over the batch
+        for index in range(batch_shape[0]):
+            # Get an image from the batch and change axis directions to match normal specification (x, y, n_channels)
+            image = np.transpose(batch[index,:,:,:], [1,2,0])
+            print(image.shape)
+            image = color.rgb2lab(image)
 
-        pass
+            if self.blur:
+                image[:,:,1] = gaussian_filter(image[:,:,1], sigma)
+                image[:,:,2] = gaussian_filter(image[:,:,2], sigma)
+
+            # Transpose back to format required for the neural network and save it in the original batch
+            batch[index,:,:,:] = np.transpose(image, [2,0,1])
+
+        return batch
+
+
+if __name__ == "__main__":
+    nnp = NNPreprocessor(5, "testing_files", blur=True)
+
+    # Test processing of a single batch
+    testbatch = np.load(os.path.join("testing_files", "testbatch.npy"))
+    nnp._process_batch(testbatch)
